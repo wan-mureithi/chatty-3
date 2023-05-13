15422362f7f5e0a2e0504778250a4ab721955c6f
wan.mureithi <estherdee27@gmail.com>
2023-05-13 12:46:46 +0300

    blog section

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,9 @@
+import blog01 from '../../assets/blogTunnel.png'
+import blog02 from '../../assets/blogAI.png'
+import blog03 from '../../assets/blogchain.png'
+import blog04 from '../../assets/blogBinary.png'
+import blog05 from '../../assets/blogError.png'
+
+export {
+    blog01, blog02, blog03, blog04, blog05
+}
